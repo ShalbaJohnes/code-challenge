198298e23812aa44a75553a8e1d1fdf1d65c97d6
ShalbaJohnes <wawerudebbie@gmail.com>
2025-09-10 06:03:50 +0300

    added code to script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appendChild(h2);
+function gradeCalculator(){
+    let marks = prompt("Enter student marks(0-100):"); //Prompts the user for input
+}
+marks = parseFloat(marks); //Convert the input to a number
+if(isNaN(marks)|| marks< 0 || marks > 100){
+    alert("Invalid input.Please enter a number between 0 and 100.");
+    return; // Exits the function if the input is void
+}
+  
+let grade;
+if (marks > 79){
+    grade = "A";
+}
+else if (marks >= 60){
+    grade = "B";
+}
+else if (marks >= 49){
+    grade = "C";
+}
+else if (marks >= 40){
+    grade = "D";
+}
+else {
+    grade = "E";
+}
